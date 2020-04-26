fc212b226454528ecd2114af8c5d08c74afd3567
xtt5221 <1395408068@qq.com>
2020-04-26 22:51:16 +0800

    完成素材管理的图片素材

--- src/views/image/index.vue
+++ src/views/image/index.vue
@@ -14,6 +14,27 @@
           <el-radio-button label="收藏"></el-radio-button>
         </el-radio-group>
       </div>
+      <!-- 图片列表 -->
+      <el-row :gutter="10">
+        <el-col :xs="12" :sm="6" :md="6" :lg="4">
+          <el-image style="height: 100px" src="https://fuss10.elemecdn.com/e/5d/4a731a90594a4af544c0c25941171jpeg.jpeg" fit="cover"></el-image>
+        </el-col>
+        <el-col :xs="12" :sm="6" :md="6" :lg="4">
+          <el-image style="height: 100px" src="https://fuss10.elemecdn.com/e/5d/4a731a90594a4af544c0c25941171jpeg.jpeg" fit="cover"></el-image>
+        </el-col>
+        <el-col :xs="12" :sm="6" :md="6" :lg="4">
+          <el-image style="height: 100px" src="https://fuss10.elemecdn.com/e/5d/4a731a90594a4af544c0c25941171jpeg.jpeg" fit="cover"></el-image>
+        </el-col>
+        <el-col :xs="12" :sm="6" :md="6" :lg="4">
+          <el-image style="height: 100px" src="https://fuss10.elemecdn.com/e/5d/4a731a90594a4af544c0c25941171jpeg.jpeg" fit="cover"></el-image>
+        </el-col>
+        <el-col :xs="12" :sm="6" :md="6" :lg="4">
+          <el-image style="height: 100px" src="https://fuss10.elemecdn.com/e/5d/4a731a90594a4af544c0c25941171jpeg.jpeg" fit="cover"></el-image>
+        </el-col>
+        <el-col :xs="12" :sm="6" :md="6" :lg="4">
+          <el-image style="height: 100px" src="https://fuss10.elemecdn.com/e/5d/4a731a90594a4af544c0c25941171jpeg.jpeg" fit="cover"></el-image>
+        </el-col>
+      </el-row>
     </el-card>
   </div>
 </template>
@@ -25,7 +46,7 @@ export default {
   props: {},
   data () {
     return {
-      radio1: '上海'
+      radio1: '全部'
     }
   },
   computed: {},
