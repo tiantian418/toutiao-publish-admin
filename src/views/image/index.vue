78f888e38abe614e0941dcfb4dfc0cb733b574f8
xtt5221 <1395408068@qq.com>
2020-04-27 23:01:16 +0800

    完成了添加素材图片

--- src/views/image/index.vue
+++ src/views/image/index.vue
@@ -7,55 +7,98 @@
             <el-breadcrumb-item to="/">首页</el-breadcrumb-item>
             <el-breadcrumb-item>素材管理</el-breadcrumb-item>
           </el-breadcrumb>
-        </div>
-      <div style="padding-bottom:20px;">
-        <el-radio-group v-model="radio1" size="mini">
-          <el-radio-button label="全部"></el-radio-button>
-          <el-radio-button label="收藏"></el-radio-button>
+      </div>
+      <div class="action-head">
+        <el-radio-group v-model="collect" size="mini" @change="onCollectChange">
+          <el-radio-button :label="false">全部</el-radio-button>
+          <el-radio-button :label="true">收藏</el-radio-button>
         </el-radio-group>
+        <el-button size="mini" type="success" @click="dialogUploadVisible = true">添加素材</el-button>
       </div>
       <!-- 图片列表 -->
       <el-row :gutter="10">
-        <el-col :xs="12" :sm="6" :md="6" :lg="4">
-          <el-image style="height: 100px" src="https://fuss10.elemecdn.com/e/5d/4a731a90594a4af544c0c25941171jpeg.jpeg" fit="cover"></el-image>
-        </el-col>
-        <el-col :xs="12" :sm="6" :md="6" :lg="4">
-          <el-image style="height: 100px" src="https://fuss10.elemecdn.com/e/5d/4a731a90594a4af544c0c25941171jpeg.jpeg" fit="cover"></el-image>
-        </el-col>
-        <el-col :xs="12" :sm="6" :md="6" :lg="4">
-          <el-image style="height: 100px" src="https://fuss10.elemecdn.com/e/5d/4a731a90594a4af544c0c25941171jpeg.jpeg" fit="cover"></el-image>
-        </el-col>
-        <el-col :xs="12" :sm="6" :md="6" :lg="4">
-          <el-image style="height: 100px" src="https://fuss10.elemecdn.com/e/5d/4a731a90594a4af544c0c25941171jpeg.jpeg" fit="cover"></el-image>
-        </el-col>
-        <el-col :xs="12" :sm="6" :md="6" :lg="4">
-          <el-image style="height: 100px" src="https://fuss10.elemecdn.com/e/5d/4a731a90594a4af544c0c25941171jpeg.jpeg" fit="cover"></el-image>
-        </el-col>
-        <el-col :xs="12" :sm="6" :md="6" :lg="4">
-          <el-image style="height: 100px" src="https://fuss10.elemecdn.com/e/5d/4a731a90594a4af544c0c25941171jpeg.jpeg" fit="cover"></el-image>
+        <el-col :xs="12" :sm="6" :md="6" :lg="4" v-for="(img, index) in images" :key="index">
+          <el-image style="height: 100px" :src="img.url" fit="cover">
+          </el-image>
         </el-col>
       </el-row>
+      <!-- 列表分页 -->
+      <el-pagination style="margin-top:15px;" background layout="prev, pager, next" :total="40"> </el-pagination>
     </el-card>
+    <el-dialog title="上传素材" :visible.sync="dialogUploadVisible" :append-to-body="true">
+      <el-upload class="upload-demo" drag action="http://ttapi.research.itcast.cn/mp/v1_0/user/images" :headers="uploadHeaders" name="image" multiple :show-file-list="false" :on-success="onUploadSuccess">
+        <i class="el-icon-upload"></i>
+        <div class="el-upload__text">将文件拖到此处，或<em>点击上传</em></div>
+        <div class="el-upload__tip" slot="tip">只能上传jpg/png文件，且不超过500kb</div>
+      </el-upload>
+    </el-dialog>
   </div>
 </template>
 
 <script>
+import { getImages } from '@/api/image'
+
 export default {
   name: 'ImageIndex',
   components: {},
   props: {},
   data () {
+    const user = JSON.parse(window.localStorage.getItem('user'))
     return {
-      radio1: '全部'
+      collect: false, // 默认查询全部图片列表
+      images: [], // 图片列表
+      dialogUploadVisible: false,
+      uploadHeaders: {
+        Authorization: `Bearer ${user.token}`
+      }
+      // pageSize: 6, // 每页大小
+      // totalCount: 0, // 总数据条数
+      // page: 1 // 当前页码
     }
   },
   computed: {},
   watch: {},
-  created () {},
+  created () {
+    this.loadImages(false)
+  },
   mounted () {},
-  methods: {}
+  methods: {
+    loadImages (collect = false) {
+      getImages({
+        collect
+        // page,
+        // per_page: this.pageSize
+      }).then(res => {
+        // console.log(res)
+        this.images = res.data.data.results
+        // this.totalCount = res.data.data.totalCount
+      })
+    },
+
+    onCollectChange (value) {
+      this.loadImages(value)
+    },
+
+    onUploadSuccess () {
+      // 关闭对话框
+      this.dialogUploadVisible = false
+
+      // 更新素材列表
+      this.loadImages(false)
+    }
+
+    // onCurrentChange (page) {
+    //   this.loadImages(page)
+    // }
+
+  }
 }
 </script>
 
 <style scoped lang='less'>
+.action-head {
+  padding-bottom: 20px;
+  display: flex;
+  justify-content: space-between;
+}
 </style>
